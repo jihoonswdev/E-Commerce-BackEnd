890aab3266c73ac28548e781da650cf1fa271ea0
Jason Park <jihoonswdev@gmail.com>
2021-10-07 15:26:05 -0400

    final changes fixed errors/bugs

--- models/index.js
+++ models/index.js
@@ -7,12 +7,12 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'Category_id',
-  onDelete: 'CASADE'
+  onDelete: 'CASCADE'
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'Category_id',
-  onDelete: 'CASADE'
+  onDelete: 'CASCADE'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,7 +3,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
@@ -16,14 +16,14 @@ router.get('/', (req, res) => {
       ]
     })
 
-    res.status(200).json(tagData);
+    res.status(200).json(tag);
   } catch (err) {
     res.status(500).json(err);
   }
   
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
@@ -43,7 +43,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tag = await Tag.create(req.body);
@@ -55,7 +55,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tag = await Tag.update(req.body, {
@@ -71,7 +71,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tag = await Tag.destroy({
