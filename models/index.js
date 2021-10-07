f0de13d1552a7a71be045a8226b5156b8aab3760
Jason Park <jihoonswdev@gmail.com>
2021-10-07 10:44:35 -0400

    updating index.js in model

--- models/index.js
+++ models/index.js
@@ -5,11 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "Category_id",
+  onDelete: "CASADE"
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "Category_id",
+})
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongsToMany(Tag, {
+  
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
